6e0bcabe134fdbd7b6d7de88f6e94f7f7f1a02c6
AnneSWarlaumont <anne.warlaumont@gmail.com>
2016-09-02 11:57:58 -0700

    Reviewed spkrcode.R and added notes on issues with the program. Found one potentially serious issue with the recoding that should be addressed. Also added a short description and link to transcription instructions to the README.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ R Scripts for WWTranscription project
 
 runanalyses.R: Run this to recode the transcriptions into categories for analysis, construct an event series based on the utterance time series (dealing with pauses and overlaps), and then create pairs of consecutive events.
 
-spkrcode.R:
+spkrcode.R: Recodes more detailed transcriptions (exported from ELAN) into single-character codes particular to the analyses for this project. Transcription instructions are available here: https://drive.google.com/folderview?id=0B7TInaKaaLhcflBDeEMyc3Z4bklMdW1nNUJmQzVlVnl5LUxyaEVPZl8tSHMyY0gzWTI3Q2c&usp=sharing
 
 pauseoverlap.R:
 
--- spkrcode.R
+++ spkrcode.R
@@ -1,14 +1,15 @@
 spkrcode = function(path, file) {
+	
 	setwd(path)
 	annotations = read.table(file,sep="\t",quote="",header=TRUE,col.names=c("start","end","InfIPA","AdultIPA","ortho","dir"),colClasses = c("numeric", "numeric", "character", "character","character", "character"))
 
 	annotations$Spkr = annotations$InfIPA!=""
 	annotations$Spkr[annotations$Spkr==TRUE] = "I"
 	annotations$Spkr[annotations$Spkr=="FALSE"] = "A"
-	annotations$Spkr[annotations$Spkr=="I"]
-	annotations$SpkrCode 
+	annotations$Spkr[annotations$Spkr=="I"] # ASW: This line doesn't do anything, just displays info.
+	annotations$SpkrCode # ASW: This line also doesn't do anything. I'm also not sure that annotations$SpkrCode is defined.
 	
-	# change annotations$Spkr to have the vocal type
+	# change annotations$Spkr to have the vocal type, putting the results in annotations$SpkrCode
 	for (crow in 1:nrow(annotations)) {
 		if (annotations$Spkr[crow] == "A") {
 			if (annotations$dir[crow] == "N") {
@@ -18,9 +19,8 @@ spkrcode = function(path, file) {
 			} else if (annotations$dir[crow] == "U") {
 				annotations$SpkrCode[crow] = "U"	
 			} 
-		
 		}
-		else if (annotations$Spkr[crow] == "I") {
+		else if (annotations$Spkr[crow] == "I") { # ASW: What would happen if the infant utterance contains a combination of the types below? It looks like if an utterance happened to contain both "x" and "cry" for example, the utterance would be tagged as "C" (canonical). That's definitely not the intention. And my understanding of the human annotation protocol is that it was possible for an utterance to be labeled as both "x" and "cry" or any other combination. It would be better not to use a blanket else for "C", but instead to look for instances of "/". Then there could be a final else that lets us know when the annotation didn't match any of the above criteria. That'll let us know if updates to the rules are needed or if there were errors in the annotation format that need correcting in the original .eaf files.
 			 if (annotations$InfIPA[crow] == "x" | annotations$InfIPA[crow] == "fuss" | annotations$InfIPA[crow] == "yell" | annotations$InfIPA[crow] == "scream" | annotations$InfIPA[crow] == "squeal" | annotations$InfIPA[crow] == "grunt" | (grepl("/", annotations$InfIPA[crow]) && !grepl("[wrtypasdfghjklzcvbnm]", annotations$InfIPA[crow]))){
 				annotations$SpkrCode[crow] = "X"
 			} else if (annotations$InfIPA[crow] == "cry" | annotations$InfIPA[crow] == "laugh" ) {
