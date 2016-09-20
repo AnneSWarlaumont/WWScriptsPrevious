0a135bc1fb3196445e40e053c5fee70d1091c452
AnneSWarlaumont <anne.warlaumont@gmail.com>
2016-09-20 08:48:14 -0700

    Made some minor formatting changes to sequenceevents.R and made edits and additions to the README.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # WWScripts
-R Scripts for WWTranscription project
+R Code for the WWTranscription project.
 
 runanalyses.R: Run this to recode the transcriptions into categories for analysis, construct an event series based on the utterance time series (dealing with pauses and overlaps), and then create pairs of consecutive events.
 
@@ -7,4 +7,10 @@ spkrcode.R: Recodes more detailed transcriptions (exported from ELAN) into singl
 
 pauseoverlap.R: Converts the ordered annotations to a sequence of non-overlapping events, with pause events inserted.
 
-sequenceevents.R:
+sequenceevents.R: Converts the sequence of non-overlapping events that was output by pauseOverlap.R into a series of two-event sequences. For example, C,C,P,X,T,X,N would be converted to CC,CP,PX,XT,TX,XN.
+
+Regression folder: Contains R code for the regression-based inferential stats for this project, as well as the sequencecodes.csv data file on which those analyses are based. (Needs to be updated)
+
+Reliability folder: Contains R and Python code for calculating the reliability of the human judgments for this project. (Needs to be cleaned up and/or updated)
+
+(There is also code for calculating descriptive statistics, doing odds ratio analyses, making plots, etc. that should be added.)
--- sequenceevents.R
+++ sequenceevents.R
@@ -1,22 +1,19 @@
-sequenceevents = function(events, InfantID, SegmentID, FileName) {
-	
-	#set first sequence (similar to event 1 in pauseoverlap)
-	#write the remaining sequences	
+sequenceevents = function(events, InfantID, SegmentID, FileName) {	
 
 	for (crow in 1:(nrow(events)-1)) {
-		if (fiveminnum == 1 & crow == 1 ) {
+		if (fiveminnum == 1 & crow == 1 ) { #set first sequence
 			sequencecodes = data.frame(InfantID=InfantID,
-								SegmentID=SegmentID,
-								FileName=FileName,
-								code1=events[1,]$SpkrCode,
-								code2=events[2,]$SpkrCode, 
-								seqcode=paste(events[1,]$SpkrCode,events[2,]$SpkrCode, sep = "", collapse = NULL),
-								stringsAsFactors=FALSE)
-		} else {
+									   SegmentID=SegmentID,
+	   								   FileName=FileName,
+									   code1=events[1,]$SpkrCode,
+									   code2=events[2,]$SpkrCode, 
+									   seqcode=paste(events[1,]$SpkrCode,events[2,]$SpkrCode, sep = "", collapse = NULL),
+									   stringsAsFactors=FALSE)
+		} else { #write the remaining sequences
 			sequencecodes = rbind(sequencecodes,c(InfantID, SegmentID, FileName, events[crow,]$SpkrCode, events[crow+1,]$SpkrCode, paste(events[crow,]$SpkrCode,events[crow+1,]$SpkrCode, sep = "", collapse = NULL)))	
-		}
+		}	
+	}	
 	
-}	
 	return(sequencecodes)
 		
 }
